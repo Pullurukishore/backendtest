bb1023ed747dbaf5b266bd60afe3e3a811755bc1
Pullurukishore <kishorereddypullur123@gmail.com>
2025-01-24 10:35:45 +0530

    Add files via upload

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser, // Include browser globals
+        ...globals.node, // Include Node.js globals
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+];
